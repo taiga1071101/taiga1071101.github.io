68e5fccc9fa9c7a264cac0c1611b9488071b1242
taiga1071101 <taigatukimi@gmail.com>
2025-02-16 19:15:31 +0900

    index.js微修正

--- server/index.js
+++ server/index.js
@@ -3,11 +3,9 @@ const app = express();
 const PORT = 9000;
 
 app.get('/', (_req, res) => {
-  res.send('Hello World!');
+  res.send('Hello World');
 });
 
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });
-
-export default app;
